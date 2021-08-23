f2245fb3b385b873021a9216f7be8ad442321ef0
Mohammad Emami <emamirazavi@gmail.com>
2021-08-23 10:53:20 +0430

    adds some forgotten items

--- config.sample.php
+++ config.sample.php
@@ -3,7 +3,7 @@ return [
     'base_url' => 'http://localhost/lab/education/biography/',
     'email_subject' => 'Biography contact us',
     'smtp_user' => 'example@gmail.com',
-    'smtp_pass'=> '*************',
+    'smtp_pass' => '*************',
     'email' => 'emamirazavi@gmail.com',
     'name' => 'S.Mohammad Emami Razavi',
     'job_title' => 'Lead and Sr. developer',
@@ -17,4 +17,18 @@ return [
     'location' => '78 Montazeri
     <br />
     Givi, Ardabil, Iran, <br /> 5685145743',
+    'portfolios' => [
+        [
+            'src' => 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png',
+            'title' => 'Ey BABA',
+            'desc' => 'Damet Garm!!! bia sitam ro bebin: <a href="https://github.com/emamirazavi" target="_blank">sitam!!!</a>',
+        ]
+    ],
+    'about' => <<<PARSA
+    I'm studying about php programming.
+    <br/>
+    I'm eager to know more and more about technology.
+    <br/>
+    I've loved computer programming until chilhood!
+    PARSA,
 ];
