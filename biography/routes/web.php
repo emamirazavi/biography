8c2bc9c2c47f5501a8d71a31f1642ebd5cea9a9d
Mohammad Emami <emamirazavi@gmail.com>
2021-08-23 11:42:59 +0430

    adds controller and model

--- biography/routes/web.php
+++ biography/routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/', "indexController");
+
+// use App\Http\Controllers\InPhotoController;
+Route::resource('bio', BioController::class);
